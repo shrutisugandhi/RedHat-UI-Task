7b57ca828cd732f4f650349db550821a1e93cebe
shrutisugandhi <shrutisugandhi07@gmail.com>
2021-10-19 12:23:20 +0530

    Fix- Css for Responsive
    Css Added for responsive layout

--- git-user-search/src/Components/SearchUser/SearchUser.jsx
+++ git-user-search/src/Components/SearchUser/SearchUser.jsx
@@ -51,74 +51,75 @@ const SearchUser = React.memo(({ userName }) => {
       <h4>
         {searchTerm ? <p>{searchTerm}-RepoList</p> : " All User Repos List"}
       </h4>
-      <table className="Repo_List">
-        <thead>
-          <tr>
-            <th>Owner Name</th>
-            <th>Repo Name</th>
-            <th>Repos Url</th>
-            <th>Description</th>
-            <th>Stars</th>
-            <th>Open Issue Count</th>
-            <th>Watchers</th>
-          </tr>
-        </thead>
-        <tbody>
-          {searchTerm.length > 0
-            ? React.Children.toArray(
-                foundUser.map((i) => {
-                  console.log(i);
-                  return (
-                    <tr>
-                      <td className="owner_details">
-                        <img
-                          src={i.owner.avatar_url}
-                          alt="owner_img"
-                          className="owner_img"
-                        ></img>
-                        <span className="owner_name"> {i.owner.login}</span>
-                      </td>
-                      <td>
-                        {" "}
-                        <span className="owner_name"> {i.name}</span>
-                      </td>
-                      <td>
-                        <a href={i.url}>{i.url}</a>
-                      </td>
-                      <td>
-                        {" "}
-                        <span className="owner_name"> {i.description}</span>
-                      </td>
-                      <td>{i.stargazers_count}</td>
-                      <td>{i.open_issues_count}</td>
-                      <td>{i.watchers_count}</td>
-                    </tr>
-                  );
-                })
-              )
-            : React.Children.toArray(
-                userName.map((i) => {
-                  return (
-                    <tr>
-                      <td>
-                        <span className="owner_name"> {i.login}</span>
-                      </td>
-                      <td>
-                        <a href={i.url}>{i.url}</a>
-                      </td>
-                      <td>
-                        <a href={i.repos_url}>{i.repos_url}</a>
-                      </td>
-                      <td>NA</td>
-                      <td>NA</td>
-                      <td>NA</td>
-                      <td>NA</td>
-                    </tr>
-                  );
-                })
-              )}
+      <div style={{ overflowX: "auto" }}>
+        <table className="Repo_List">
+          <thead>
+            <tr>
+              <th>Owner Name</th>
+              <th>Repo Name</th>
+              <th>Repos Url</th>
+              <th>Description</th>
+              <th>Stars</th>
+              <th>Open Issue Count</th>
+              <th>Watchers</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchTerm.length > 0
+              ? React.Children.toArray(
+                  foundUser.map((i) => {
+                    console.log(i);
+                    return (
+                      <tr>
+                        <td className="owner_details">
+                          <img
+                            src={i.owner.avatar_url}
+                            alt="owner_img"
+                            className="owner_img"
+                          ></img>
+                          <span className="owner_name"> {i.owner.login}</span>
+                        </td>
+                        <td>
+                          {" "}
+                          <span className="owner_name"> {i.name}</span>
+                        </td>
+                        <td>
+                          <a href={i.url}>{i.url}</a>
+                        </td>
+                        <td>
+                          {" "}
+                          <span className="owner_name"> {i.description}</span>
+                        </td>
+                        <td>{i.stargazers_count}</td>
+                        <td>{i.open_issues_count}</td>
+                        <td>{i.watchers_count}</td>
+                      </tr>
+                    );
+                  })
+                )
+              : React.Children.toArray(
+                  userName.map((i) => {
+                    return (
+                      <tr>
+                        <td>
+                          <span className="owner_name"> {i.login}</span>
+                        </td>
+                        <td>
+                          <a href={i.url}>{i.url}</a>
+                        </td>
+                        <td>
+                          <a href={i.repos_url}>{i.repos_url}</a>
+                        </td>
+                        <td>NA</td>
+                        <td>NA</td>
+                        <td>NA</td>
+                        <td>NA</td>
+                      </tr>
+                    );
+                  })
+                )}
 
-          {/* {searchTerm.length > 0
+            {/* {searchTerm.length > 0
             ? foundUser.map((i) => {
                 return (
                   <tr>
@@ -139,8 +140,9 @@ const SearchUser = React.memo(({ userName }) => {
                   </tr>
                 );
               })} */}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </React.Fragment>
   );
 });
