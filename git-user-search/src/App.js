1ec2d71f67c7ed6f0f22a45de7ae0ad475ddba9f
shrutisugandhi <shrutisugandhi07@gmail.com>
2021-10-19 11:35:20 +0530

    feat-Toggle between All User & searched User Repo
    Toggle Between All User & Searched User Repo

--- git-user-search/src/App.js
+++ git-user-search/src/App.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import debounce from "lodash.debounce";
 import "./App.css";
 import SearchUser from "./Components/SearchUser/SearchUser";
 
 function App() {
+  const [RepoList, setRepoList] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setRepoList(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // const getRepoName = () => {
+  //   // const response = await fetch("https://api.github.com/users");
+  //   // const Repos = await response.json();
+  //   // console.log("Lst of users", Repos);
+  //   // setRepoList({
+  //   //   ...RepoList,
+  //   //   Repos,
+  //   // });
+  //   // console.log("RepoList of", RepoList);
+  //   //   return Repos;
+
+  //   fetch("https://api.github.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setRepoList(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
   return (
     <div className="App">
-      <SearchUser></SearchUser>
+      <h5 style={{ color: "blue" }}>
+        Toggle Between All User Repo and searched Repo list based on Search
+      </h5>
+      <SearchUser userName={RepoList}></SearchUser>
     </div>
   );
 }
--- git-user-search/src/Components/SearchUser/SearchUser.jsx
+++ git-user-search/src/Components/SearchUser/SearchUser.jsx
@@ -1,105 +1,144 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import debounce from "lodash.debounce";
 import "./SearchUser.css";
-const SearchUser = React.memo(() => {
-  const [items, setRepoList] = useState([]);
+const SearchUser = React.memo(({ userName }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [foundUser, setFoundUser] = useState([]);
-  useEffect(() => {
-    getRepoName();
-  }, []);
-  const getRepoName = async () => {
-    // const response = await fetch("https://api.github.com/users");
-    // const Repos = await response.json();
-    // console.log("Lst of users", Repos);
-    // setRepoList({
-    //   ...RepoList,
-    //   Repos,
-    // });
-    // console.log("RepoList of", RepoList);
-    //   return Repos;
 
-    fetch("https://api.github.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setRepoList(data);
-        setFoundUser(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  //console.log("RepoList>>", items);
+  console.log("userName>>", userName);
 
   const handleUserInput = (e) => {
-    const keyword = e.target.value;
-    if (keyword !== "") {
-      console.log("keyword>>", keyword);
-      const result = items.filter((item, index) => {
-        console.log("items>>", index, ">>>", item);
-        return item.login.toLowerCase().startsWith(keyword.toLowerCase());
-      });
-      console.log("result", result);
-      setFoundUser(result);
-      console.log("foundUser>.", foundUser);
-    } else {
-      setFoundUser(items);
-    }
-    setSearchTerm(keyword);
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+    getUser(searchTerm);
   };
-
+  console.log("search>>", searchTerm);
   const debouncedChangeHandler = useCallback(
     debounce(handleUserInput, 3000),
     []
   );
-  //   if (userInput !== "") {
-  //     listToDisplay = fruits.filter((fruit) => {
-  //       return fruit.includes(userInput);
+  const getUser = (searchTerm) => {
+    console.log("search1>>", searchTerm);
+
+    if (searchTerm.length > 0) {
+      fetch("https://api.github.com/users/" + searchTerm + "/repos")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data", data);
+          setFoundUser(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  //   if (searchTerm !== "") {
+  //     filteredNames = RepoList.filter((name) => {
+  //       return name.toLowerCase().includes(searchTerm.toLowerCase());
   //     });
   //   }
-
   return (
     <React.Fragment>
-      <input
-        type="text"
-        placeholder="Search User"
-        onChange={debouncedChangeHandler}
-      />
+      <div className="search_block">
+        <input
+          type="text"
+          placeholder="Search User"
+          onChange={debouncedChangeHandler}
+        />
+      </div>
       {/* {renderFruitList()} */}
+      <h4>
+        {searchTerm ? <p>{searchTerm}-RepoList</p> : " All User Repos List"}
+      </h4>
       <table className="Repo_List">
         <thead>
           <tr>
-            <th>Login User</th>
+            <th>Owner Name</th>
+            <th>Repo Name</th>
             <th>Repos Url</th>
+            <th>Description</th>
+            <th>Stars</th>
+            <th>Open Issue Count</th>
+            <th>Watchers</th>
           </tr>
         </thead>
         <tbody>
-          {/* {Repos.map((i) => {
-            console.log(i);
-            return (
-              <tr>
-                <td>{i.login}</td>{" "}
-                <td>
-                  <a href={i.repos_url}>{i.repos_url}</a>
-                </td>
-              </tr>
-            );
-          })} */}
-          {foundUser && foundUser.length > 0
+          {searchTerm.length > 0
             ? React.Children.toArray(
                 foundUser.map((i) => {
+                  console.log(i);
                   return (
                     <tr>
-                      <td>{i.login}</td>
-                      <td>
-                        <a href={i.repos_url}>{i.repos_url}</a>
+                      <td className="owner_details">
+                        <img
+                          src={i.owner.avatar_url}
+                          alt="owner_img"
+                          className="owner_img"
+                        ></img>
+                        <span className="owner_name"> {i.owner.login}</span>
                       </td>
+                      <td>
+                        {" "}
+                        <span className="owner_name"> {i.name}</span>
+                      </td>
+                      <td>
+                        <a href={i.url}>{i.url}</a>
+                      </td>
+                      <td>
+                        {" "}
+                        <span className="owner_name"> {i.description}</span>
+                      </td>
+                      <td>{i.stargazers_count}</td>
+                      <td>{i.open_issues_count}</td>
+                      <td>{i.watchers_count}</td>
                     </tr>
                   );
                 })
               )
-            : "Loading..."}
+            : React.Children.toArray(
+                userName.map((i) => {
+                  return (
+                    <tr>
+                      <td>
+                        <span className="owner_name"> {i.login}</span>
+                      </td>
+                      <td>
+                        <a href={i.url}>{i.url}</a>
+                      </td>
+                      <td>
+                        <a href={i.repos_url}>{i.repos_url}</a>
+                      </td>
+                      <td>NA</td>
+                      <td>NA</td>
+                      <td>NA</td>
+                      <td>NA</td>
+                    </tr>
+                  );
+                })
+              )}
+
+          {/* {searchTerm.length > 0
+            ? foundUser.map((i) => {
+                return (
+                  <tr>
+                    <td>{i.name}</td>
+                    <td>
+                      <a href={i.full_name}>{i.full_name}</a>
+                    </td>
+                  </tr>
+                );
+              })
+            : RepoList.map((i) => {
+                return (
+                  <tr>
+                    <td>{i.login}</td>
+                    <td>
+                      <a href={i.repos_url}>{i.repos_url}</a>
+                    </td>
+                  </tr>
+                );
+              })} */}
         </tbody>
       </table>
     </React.Fragment>
